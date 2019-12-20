defe63edea79881cbf1cbb0ad3db2068773e6695
hibitomo <hibi.tomoya@lagopus.org>
2019-12-20 08:19:41 +0000

    Add setup_proxy.sh

---
+++ test_env/setup_proxy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+HTTP_PROXY="http://example.com/"
+HTTPS_PROXY="https://example.com/"
+
+ANSIBLE_PLAYBOOK=./ansible-Proxy/setup.yml
+TMP_INVENTORY="/tmp/setup_hosts"
+
+
+echo "[server]" > ${TMP_INVENTORY}
+
+
+cat <<EOF > ./ansible-Proxy/roles/proxy/vars/main.yml
+http_proxy: "${HTTP_PROXY}"
+https_proxy: "${HTTPS_PROXY}"
+EOF
+
+for i in $@ ; do
+
+    echo "####### Setup Proxy: ${i} ######"
+
+    INSTANCE_IP=`sudo uvt-kvm ip ${i}`
+    if [ $? != 0 ] ; then
+	continue
+    fi
+
+    ssh-keygen -R ${INSTANCE_IP}
+
+    uvt-kvm ssh ${i} "sudo echo 'Acquire::http::Proxy \"${HTTP_PROXY}\";' > apt.conf" --insecure
+    uvt-kvm ssh ${i} "sudo echo 'Acquire::https::Proxy \"${HTTPS_PROXY}\";' >> apt.conf" --insecure
+    uvt-kvm ssh ${i} "sudo mv apt.conf /etc/apt/" --insecure
+    uvt-kvm ssh ${i} "sudo apt-get install python -y" --insecure
+
+    echo ${INSTANCE_IP} >> ${TMP_INVENTORY}
+
+done
+
+ansible-playbook -i ${TMP_INVENTORY} ${ANSIBLE_PLAYBOOK}
