a9758516d0cba48c01e75a7e70d7fd0b8cf2d231
hibitomo <hibi.tomoya@lagopus.org>
2019-12-22 11:56:50 +0000

    Fix setup_proxy.sh

--- test_env/setup_proxy.sh
+++ test_env/setup_proxy.sh
@@ -7,12 +7,13 @@ ANSIBLE_PLAYBOOK=./ansible-Proxy/site.yml
 TMP_INVENTORY="/tmp/setup_hosts"
 
 
-echo "[server]" > ${TMP_INVENTORY}
+echo "[hosts1]" > ${TMP_INVENTORY}
 
 
-cat <<EOF > ./ansible-Proxy/roles/proxy/vars/main.yml
-http_proxy: "${HTTP_PROXY}"
-https_proxy: "${HTTPS_PROXY}"
+cat <<EOF > ./ansible-Proxy/vars/main.yml
+proxy_env:
+  http_proxy: "${HTTP_PROXY}"
+  https_proxy: "${HTTPS_PROXY}"
 EOF
 
 for i in $@ ; do
@@ -25,11 +26,6 @@ for i in $@ ; do
     fi
 
     ssh-keygen -R ${INSTANCE_IP}
-
-    uvt-kvm ssh ${i} "sudo echo 'Acquire::http::Proxy \"${HTTP_PROXY}\";' > apt.conf" --insecure
-    uvt-kvm ssh ${i} "sudo echo 'Acquire::https::Proxy \"${HTTPS_PROXY}\";' >> apt.conf" --insecure
-    uvt-kvm ssh ${i} "sudo mv apt.conf /etc/apt/" --insecure
-    uvt-kvm ssh ${i} "sudo apt-get install python -y" --insecure
 
     echo ${INSTANCE_IP} >> ${TMP_INVENTORY}
 
