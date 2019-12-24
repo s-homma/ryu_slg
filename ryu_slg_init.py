d3d4f2ee6a3cff76828d5006402bd14dad66278b
hibitomo <hibi.tomoya@lagopus.org>
2019-12-24 15:34:15 +0000

    Fix ryu_slg_init.py

--- ryu_slg_init.py
+++ ryu_slg_init.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Nippon Telegraph and Telephone Corporation.
+# Copyright (C) 2019 Nippon Telegraph and Telephone Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,34 +26,40 @@ from ryu.lib.ofctl_v1_3 import mod_flow_entry
 
 
 
-####################################
+#####################################################################################################################
 #
-# * Port assignment of SliceGateway:
-#
-# port 1: Downlink interface
-# port 2: Uplink/VxLAN interface
-# port 3: Copy of downlink interface to reply icmp and arp
-# port 4: Copy of uplink interface to reply icmp and arp
-#
-#
-#  Copy of   +        + Copy of
-#  Downlink  |        | Uplink
-#  Interface |        | Interface
-#            |        |
-# +----------+--------+----------+
-# |          3        4          |
-# |                              |
-# |       Lagopus OF Switch      |
-# |                              |
-# |          1        2          |
-# +----------+--------+----------+
-#            |        |
-#  Downlink  |        | Uplink(VxLAN)
-#  Interface |        | Interface
-#            |        |
-#            +        +
-#
-#
+#  +------------------------+  +------------------------+  +------------------------+    +---------------------+
+#  |                        |  |                        |  |                        |    |                     |
+#  | VM1                    |  | VM2                    |  | VM3                    |    | VM4                 |
+#  | Role: Local Router     |  | Role: MEC              |  | Role: Central DC       |    | Role: Router        |
+#  | MAC: 52:54:00:01:00:01 |  | MAC: 52:54:00:02:00:01 |  | MAC: 52:54:00:03:00:01 |    | SLG01-if:           |
+#  | IP:  192.168.42.253    |  | IP:  192.168.1.50      |  | IP:  192.168.3.50      |    |   172.16.1.254      |
+#  |                        |  |                        |  |                        |    |   52:54:00:00:01:01 |
+#  +-------+----------------+  +-------+----------------+  +-------+----------------+    | SLG02-if:           |
+#          |                           |                           |                     |   172.16.2.254      |
+#          |         +-------------------------------------------------------------------+   52:54:00:00:01:02 |
+#          |         |                 |                           |                     | SLG03-if:           |
+#          |         |                 |        +----------------------------------------+   172.16.3.254      |
+#          |         |                 |        |                  |                     |   52:54:00:00:01:03 |
+#          |         |                 |        |                  |       +-------------+                     |
+#          |         |                 |        |                  |       |             +---------------------+
+#          |         |                 |        |                  |       |
+#+------------------------------------------------------------------------------------+
+#|         |         |                 |        |                  |       |          |
+#| +-------+---------+------+  +-------+--------+-------+  +-------+-------+--------+ |
+#| |                        |  |                        |  |                        | |
+#| | OF-Bridge01            |  | OF-Bridge02            |  | OF-Bridge03            | |
+#| | Role: SLG1             |  | Role: SLG2             |  | Role: SLG3             | |
+#| | Downlink:              |  | Downlink:              |  | Downlink:              | |
+#| |   192.168.42.254       |  |   192.168.1.254        |  |   192.168.3.254        | |
+#| |   52:54:00:01:01:01    |  |   52:54:00:02:01:01    |  |   52:54:00:03:01:01    | |
+#| | Uplink:                |  | Uplink:                |  | Uplink:                | |
+#| |   172.16.1.1           |  |   172.16.2.1           |  |   172.16.3.1           | |
+#| |   52:54:00:00:00:01    |  |   52:54:00:00:00:02    |  |   52:54:00:00:00:03    | |
+#| |                        |  |                        |  |                        | |
+#| +------------------------+  +------------------------+  +------------------------+ |
+#|                                                                                    |
+#+------------------------------------------------------------------------------------+
 #
 # * Table Design:
 #
@@ -67,7 +73,7 @@ from ryu.lib.ofctl_v1_3 import mod_flow_entry
 # table 103: encap vni 5030, output to uplink
 # table 104: encap vni 5040, output to uplink
 #
-#######################################
+####################################################################################################################
 
 class interface(dict):
 
@@ -75,19 +81,11 @@ class interface(dict):
         self.index = index
         self.ip_address = ip_address
         self.mac_address = mac_address
-        self.neigh = {}
-        self.gw = {}
+        self.gateway = None
         return
 
     def __repl__(self):
         return str({"index": self.index, "ip_address": self.ip_address, "mac_address": self.mac_address})
-
-    def add_neigh(self, ip_address, mac_address):
-        self.neigh[ip_address] = mac_address
-        return
-    def add_gw(self, ip_address, mac_address):
-        self.gw = {"ip_address": ip_address, "mac_address": mac_address}
-        return
 
 class SliceGateway(dict):
 
@@ -102,12 +100,8 @@ class SliceGateway(dict):
         self.interface[index] = interface(index, ip_address, mac_address)
         return
 
-    def add_neigh(self, index, ip_address, mac_address):
-        self.interface[index].add_neigh(ip_address, mac_address)
-        return
-
     def add_gw(self, index, ip_address, mac_address):
-        self.interface[index].add_gw(ip_address, mac_address)
+        self.interface[index].gateway = interface(0, ip_address, mac_address)
 
     
 slg = [
@@ -116,21 +110,20 @@ slg = [
     SliceGateway()
 ]
 
-slg[0].add_interface(0, "172.16.0.254", "12:34:56:78:9a:bc")
-slg[0].add_gw(0, "172.16.0.253", "12:34:56:78:9a:bc")
-slg[0].add_interface(1, "172.16.1.1", "12:34:56:78:9a:bc")
-slg[0].add_gw(1, "172.16.1.254", "12:34:56:78:9a:bc")
+slg[0].add_interface(1, "192.168.42.254", "52:54:00:01:01:01")
+slg[0].add_gw(1, "192.168.42.253", "52:54:00:01:00:01")
+slg[0].add_interface(2, "172.16.1.1", "52:54:00:00:00:01")
+slg[0].add_gw(2, "172.16.1.254", "52:54:00:00:01:01")
 
-slg[1].add_interface(0, "192.168.1.254", "12:34:56:78:9a:bc")
-slg[1].add_gw(0, "192.168.1.50", "12:34:56:78:9a:bc")
-slg[1].add_interface(1, "172.16.2.1", "12:34:56:78:9a:bc")
-slg[1].add_gw(1, "172.16.2.254", "12:34:56:78:9a:bc")
+slg[1].add_interface(1, "192.168.1.254", "52:54:00:02:01:01")
+slg[1].add_gw(1, "192.168.1.50", "52:54:00:02:00:01")
+slg[1].add_interface(2, "172.16.2.1", "52:54:00:00:00:02")
+slg[1].add_gw(2, "172.16.2.254", "52:54:00:00:01:02")
 
-slg[2].add_interface(0, "192.168.3.254", "12:34:56:78:9a:bc")
-slg[2].add_gw(0, "192.168.3.50", "12:34:56:78:9a:bc")
-slg[2].add_interface(1, "172.16.3.1", "12:34:56:78:9a:bc")
-slg[2].add_gw(1, "172.16.3.254", "12:34:56:78:9a:bc")
-
+slg[2].add_interface(1, "192.168.3.254", "52:54:00:03:01:01")
+slg[2].add_gw(1, "192.168.3.50", "52:54:00:03:00:01")
+slg[2].add_interface(2, "172.16.3.1", "52:54:00:00:00:03")
+slg[2].add_gw(2, "172.16.3.254", "52:54:00:00:01:03")
 
 PACKET_TYPE_ETHER=0
 PACKET_TYPE_IPv4=67584
@@ -144,126 +137,70 @@ class RyuSlgInit(app_manager.RyuApp):
     def __init__(self, *args, ** kwargs):
         super(RyuSlgInit, self).__init__(*args, **kwargs)
 
-
+    def make_vxlan_encap_action(self, eth_src, eth_dst, ipv4_src, ipv4_dst, udp_src, vni):
+        return [
+                {"type": "ENCAP", "packet_type": PACKET_TYPE_VxLAN},
+                {"type": "SET_FIELD", "field": "vxlan_vni", "value": vni},
+                {"type": "ENCAP", "packet_type": PACKET_TYPE_UDP},
+                {"type": "SET_FIELD", "field": "udp_src", "value": udp_src},
+                {"type": "SET_FIELD", "field": "udp_dst", "value":4789},
+                {"type": "ENCAP", "packet_type": PACKET_TYPE_IPv4},
+                {"type": "SET_FIELD", "field": "ipv4_src", "value": ipv4_src},
+                {"type": "SET_FIELD", "field": "ipv4_dst", "value": ipv4_dst},
+                {"type": "SET_NW_TTL", "nw_ttl": 64},
+                {"type": "ENCAP",  "packet_type": PACKET_TYPE_ETHER},
+                {"type": "SET_FIELD", "field": "eth_src", "value": eth_src},
+                {"type": "SET_FIELD", "field": "eth_dst", "value": eth_dst}
+        ]
+    def make_vxlan_decap_action(self):
+        return [
+            {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_ETHER, "new_pkt_type": PACKET_TYPE_IPv4},
+            {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_IPv4, "new_pkt_type": PACKET_TYPE_UDP},
+            {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_UDP, "new_pkt_type": PACKET_TYPE_VxLAN},
+            {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_VxLAN, "new_pkt_type": PACKET_TYPE_NEXT},
+        ]
+        
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         self.logger.info('switch joind: datapath: %061x' % datapath.id)
 
+        slg_id = datapath.id - 1
+        
         # base flow
         flow = {'priority': 1, 'table_id': 0}
         flow['match'] = {'in_port': 1}
-        flow['actions'] = [{'type':'OUTPUT','port': 3}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 3}
-        flow['actions'] = [{'type':'OUTPUT','port': 1}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 2}
-        flow['actions'] = [{'type':'OUTPUT','port': 4}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 4}
         flow['actions'] = [{'type':'OUTPUT','port': 2}]
         print flow
         mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
-        # ICMP
-        flow = {'priority': 10, 'table_id': 0}
-        flow['match'] = {'in_port': 1, 'dl_type': 2048,
-                         'ipv4_dst': slg[datapath.id - 1].interface[0].ip_address}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 3}]
-        print flow
-        flow['match'] = {'in_port': 3, 'dl_type': 2048}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 1}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 2, 'dl_type': 2048,
-                         'ipv4_dst': slg[datapath.id - 1].interface[1].ip_address}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 4}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 2, 'dl_type': 2048}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 4}]
+        flow['match'] = {'in_port': 2}
+        flow['actions'] = [{'type':'OUTPUT','port': 1}]
         print flow
         mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
         
-        # ARP
-        flow = {'priority': 10, 'table_id': 0}
-        flow['match'] = {'in_port': 1, 'dl_type': 2054}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 3}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow = {'priority': 10, 'table_id': 0}
-        flow['match'] = {'in_port': 3, 'dl_type': 2054}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 1}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 2, 'dl_type': 2054}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 4}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 4, 'dl_type': 2054}
-        flow['actions'] = [{'type': 'OUTPUT', 'port': 2}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-
-
-
-
-        
-        # in_port: 1
-        flow = {'priority': 5, 'table_id':0}
-        flow['match'] = {'in_port': 1}
-        flow['actions'] = [{'type':}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
-        # in_port: 2 DECAP
-        flow = {'priority': 5, 'table_id':0}
-        flow['match'] = {'in_port'}
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-
-        # Routing
-        
-
-        # OUTPUT port 1
-        flow = {'priority': 10, 'table_id': 11}
-        flow['match'] = {}
-        flow['actions'] = [{"type":"OUTPUT", "port": 1}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
-        # OUTPUT port 2
-        flow = {'priority': 10, 'table_id': 12}
-        flow["match"] = {}
-        flow["actions"] = [{"type": "OUTPUT", "port": 2}]
-        print flow
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
-        # encap vni 5000, OUTPUT port 2
-        for i in range(5):
-            flow = {'priority': 10, 'table_id': (100 + i)}
-            flow["match"] = {}
-            flow["actions"] = [
-                {"type": "ENCAP", "packet_type": PACKET_TYPE_VxLAN},
-                {"type": "SET_FIELD", "field": "vxlan_vni", "value": (5000 + i * 10)},
-                {"type": "ENCAP", "packet_type": PACKET_TYPE_UDP},
-                {"type": "SET_FIELD", "field": "udp_src", "value":5432},
-                {"type": "SET_FIELD", "field": "udp_dst", "value":4789},
-                {"type": "ENCAP", "packet_type": PACKET_TYPE_IPv4},
-                {"type": "SET_FIELD", "field": "ipv4_src", "value": slg[datapath.id - 1].interface[1].ip_address},
-                {"type": "SET_FIELD", "field": "ipv4_dst", "value": "172.21.0.2"},
-                {"type": "SET_FILED", "field": "SET_NW_TTL", "value": 64},
-                {"type": "ENCAP",  "packet_type": PACKET_TYPE_ETHER},
-                {"type": "SET_FIELD", "field": "eth_src", "value": slg[datapath.id - 1].interface[1].mac_address},
-                {"type": "SET_FIELD", "field": "eth_dst", "value": slg[datapath.id - 1].interface[1].gw["mac_address"]},
-                {"type": "OUTPUT", "port": 2}
-            ]
+        if slg_id != 1 :
+            # in_port: 1 ENCAP
+            flow = {'priority': 10, 'table_id':0}
+            flow['match'] = {'in_port': 1, "dl_type":2048}
+            flow['actions'] = self.make_vxlan_encap_action(
+                eth_src = slg[slg_id].interface[2].mac_address,
+                eth_dst = slg[slg_id].interface[2].gateway.mac_address,
+                ipv4_src = slg[slg_id].interface[2].ip_address,
+                ipv4_dst = slg[2 if slg_id != 2 else 0].interface[2].ip_address,
+                udp_src = 12345,
+                vni = 5000 + 4 * 10
+            )
+            flow['actions'].append({"type":"OUTPUT", "port": 2})
             print flow
             mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-
+        
+            # in_port: 2 DECAP
+            flow = {'priority': 10, 'table_id':0}
+            flow['match'] = {'in_port':2, 'dl_type': 2048, "nw_proto":17, "tp_dst": 4789}
+            flow['actions'] = self.make_vxlan_decap_action()
+            flow['actions'].append({"type": "SET_FIELD", "field": "eth_dst", "value": slg[0 if slg_id != 2 else 2].interface[1].gateway.mac_address})
+            flow['actions'].append({"type":"OUTPUT", "port": 1})
+            print flow
+            mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
         
