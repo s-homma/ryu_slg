c3c7708fd0c1d7b8ee5d58362afe308ae44532e7
hibitomo <hibi.tomoya@lagopus.org>
2019-12-24 17:03:58 +0000

    Fix ryu_slg.py

--- ryu_slg.py
+++ ryu_slg.py
@@ -159,6 +159,85 @@ supported_ofctl = {
 # POST /slice/reset
 #
 
+class interface(dict):
+
+    def __init__(self, index, ip_address, mac_address):
+        self.index = index
+        self.ip_address = ip_address
+        self.mac_address = mac_address
+        self.gateway = None
+        return
+
+    def __repl__(self):
+        return str({"index": self.index, "ip_address": self.ip_address, "mac_address": self.mac_address})
+
+class SliceGateway(dict):
+
+    def __init__(self):
+        self.interface = {}
+        return
+
+    def __repl__(self):
+        return self.interface
+    
+    def add_interface(self, index, ip_address, mac_address):
+        self.interface[index] = interface(index, ip_address, mac_address)
+        return
+
+    def add_gw(self, index, ip_address, mac_address):
+        self.interface[index].gateway = interface(0, ip_address, mac_address)
+
+
+slg = [
+    SliceGateway(),
+    SliceGateway(),
+    SliceGateway()
+]
+
+slg[0].add_interface(1, "192.168.42.254", "52:54:00:01:01:01")
+slg[0].add_gw(1, "192.168.42.253", "52:54:00:01:00:01")
+slg[0].add_interface(2, "172.16.1.1", "52:54:00:00:00:01")
+slg[0].add_gw(2, "172.16.1.254", "52:54:00:00:01:01")
+
+slg[1].add_interface(1, "192.168.1.254", "52:54:00:02:01:01")
+slg[1].add_gw(1, "192.168.1.50", "52:54:00:02:00:01")
+slg[1].add_interface(2, "172.16.2.1", "52:54:00:00:00:02")
+slg[1].add_gw(2, "172.16.2.254", "52:54:00:00:01:02")
+
+slg[2].add_interface(1, "192.168.3.254", "52:54:00:03:01:01")
+slg[2].add_gw(1, "192.168.3.50", "52:54:00:03:00:01")
+slg[2].add_interface(2, "172.16.3.1", "52:54:00:00:00:03")
+slg[2].add_gw(2, "172.16.3.254", "52:54:00:00:01:03")
+
+PACKET_TYPE_ETHER=0
+PACKET_TYPE_IPv4=67584
+PACKET_TYPE_UDP=131089
+PACKET_TYPE_VxLAN=201397
+PACKET_TYPE_NEXT=65534
+
+def make_vxlan_encap_action(eth_src, eth_dst, ipv4_src, ipv4_dst, udp_src, vni):
+    return [
+        {"type": "ENCAP", "packet_type": PACKET_TYPE_VxLAN},
+        {"type": "SET_FIELD", "field": "vxlan_vni", "value": vni},
+        {"type": "ENCAP", "packet_type": PACKET_TYPE_UDP},
+        {"type": "SET_FIELD", "field": "udp_src", "value": udp_src},
+        {"type": "SET_FIELD", "field": "udp_dst", "value":4789},
+        {"type": "ENCAP", "packet_type": PACKET_TYPE_IPv4},
+        {"type": "SET_FIELD", "field": "ipv4_src", "value": ipv4_src},
+        {"type": "SET_FIELD", "field": "ipv4_dst", "value": ipv4_dst},
+        {"type": "SET_NW_TTL", "nw_ttl": 64},
+        {"type": "ENCAP",  "packet_type": PACKET_TYPE_ETHER},
+        {"type": "SET_FIELD", "field": "eth_src", "value": eth_src},
+        {"type": "SET_FIELD", "field": "eth_dst", "value": eth_dst}
+    ]
+def make_vxlan_decap_action():
+    return [
+        {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_ETHER, "new_pkt_type": PACKET_TYPE_IPv4},
+        {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_IPv4, "new_pkt_type": PACKET_TYPE_UDP},
+        {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_UDP, "new_pkt_type": PACKET_TYPE_VxLAN},
+        {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_VxLAN, "new_pkt_type": PACKET_TYPE_NEXT},
+    ]
+
 
 class StatsController(ControllerBase):
     def __init__(self, req, link, data, **config):
@@ -635,6 +714,7 @@ class StatsController(ControllerBase):
 
         return Response(status=200)
 
+
     def mod_slice(self, req, cmd, **_kwargs):
 
         print "mod_slice"
@@ -646,17 +726,6 @@ class StatsController(ControllerBase):
             LOG.debug('invalid syntax %s', req.body)
             return Response(status=400)
 
-        pp = pprint.PrettyPrinter(indent=4)
-        
-        if cmd == 'add':
-            print 'add slice'
-        elif cmd == 'delete':
-            print 'delete slice'
-        elif cmd == 'reset':
-            print 'reset slice'
-        else:
-            return Response(status=404)
-
         print slice
 
         slice_id = slice.get('slice_id')
@@ -667,26 +736,135 @@ class StatsController(ControllerBase):
         print "dst_ip: " + str(dst_ip)
         dst_port = slice.get('dst_port')
         print "dst_port: " + str(dst_port)
-        if slice_id is not None:
-            vni = 5000 + slice_id * 10
-            print "vni: " + str(vni)
+        vni = 5000 + slice_id * 10
+        print "vni: " + str(vni)
 
-        flows = list()
         for i in range(3):
-            flows.append({
-                "dpid":i,
-                "match":{"eth_type":0x800, "src_ip":src_ip, "dst_ip":dst_ip, "ip_proto": 6, "dst_port":dst_port},
-                "actions":[{"vni":slice_id}]
-            })
-        pp.pprint(flows)
-            
-        # dpid=1
-        # dp = self.dpset.get(int(dpid))
-        # if dp is None:
-        #     return Response(status=404)
+            dpid = i + 1
+            dp = self.dpset.get(int(dpid))
+            if dp is None:
+                return Response(status=404)
 
-        # _ofp_version = dp.ofproto.OFP_VERSION
-        # _ofctl = supported_ofctl.get(_ofp_version, None)
+
+        ## SLG1: Edge Router Gateway
+        dpid = 1
+        dp = self.dpset.get(int(dpid))
+        _ofp_version = dp.ofproto.OFP_VERSION
+        _ofctl = supported_ofctl.get(_ofp_version, None)
+        if _ofctl is None:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+        
+        if cmd == 'add':
+            of_cmd = dp.ofproto.OFPFC_ADD
+        elif cmd == 'delete':
+            of_cmd = dp.ofproto.ofpfc_delete
+        elif cmd == 'reset':
+            print 'reset slice: Not implemented'
+            return Response(status=404)
+        else:
+            return Response(status=404)
+
+        flow = {'priority': 100, 'table_id':0}
+        flow['actions'] = make_vxlan_encap_action(
+            eth_src = slg[0].interface[2].mac_address,
+            eth_dst = slg[0].interface[2].gateway.mac_address,
+            ipv4_src = slg[0].interface[2].ip_address,
+            ipv4_dst = slg[1 if slice_id == 1 or slice_id == 2 else 2].interface[2].ip_address,
+            udp_src = 12345,
+            vni = 5000 + slice_id * 10
+        )
+        flow['actions'].append({"type":"OUTPUT", "port": 2})
+        flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 6, "ipv4_src": src_ip, "ipv4_dst": dst_ip, "tcp_dst": dst_port}
+        print flow
+        _ofctl.mod_flow_entry(dp, flow, of_cmd)
+        flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 17, "ipv4_src": src_ip, "ipv4_dst": dst_ip, "udp_dst": dst_port}
+        print flow
+        _ofctl.mod_flow_entry(dp, flow, of_cmd)
+        flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 1, "ipv4_src": src_ip, "ipv4_dst": dst_ip}
+        print flow
+        _ofctl.mod_flow_entry(dp, flow, of_cmd)
+
+
+        
+        ## SLG2: MEC gateway
+        if slice_id == 1 or slice_id == 2:
+            dpid = 2
+            dp = self.dpset.get(int(dpid))
+            _ofp_version = dp.ofproto.OFP_VERSION
+            _ofctl = supported_ofctl.get(_ofp_version, None)
+            if _ofctl is None:
+                LOG.debug('Unsupported OF protocol')
+                return Response(status=501)
+            
+            if cmd == 'add':
+                of_cmd = dp.ofproto.OFPFC_ADD
+            elif cmd == 'delete':
+                of_cmd = dp.ofproto.ofpfc_delete
+            elif cmd == 'reset':
+                print 'reset slice: Not implemented'
+                return Response(status=404)
+            else:
+                return Response(status=404)
+            
+            flow = {'priority': 100, 'table_id':0}
+            flow['actions'] = make_vxlan_encap_action(
+                eth_src = slg[1].interface[2].mac_address,
+                eth_dst = slg[1].interface[2].gateway.mac_address,
+                ipv4_src = slg[1].interface[2].ip_address,
+                ipv4_dst = slg[0].interface[2].ip_address,
+                udp_src = 12345,
+                vni = 5000 + slice_id * 10
+            )
+            flow['actions'].append({"type":"OUTPUT", "port": 2})
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 6, "ipv4_src": dst_ip, "ipv4_dst": src_ip, "udp_src": dst_port}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 17, "ipv4_src": dst_ip, "ipv4_dst": src_ip, "udp_src": dst_port}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 1, "ipv4_src": dst_ip, "ipv4_dst": src_ip}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
+
+        else:
+            dpid = 3
+            dp = self.dpset.get(int(dpid))
+            _ofp_version = dp.ofproto.OFP_VERSION
+            _ofctl = supported_ofctl.get(_ofp_version, None)
+            if _ofctl is None:
+                LOG.debug('Unsupported OF protocol')
+                return Response(status=501)
+            
+            if cmd == 'add':
+                of_cmd = dp.ofproto.OFPFC_ADD
+            elif cmd == 'delete':
+                of_cmd = dp.ofproto.ofpfc_delete
+            elif cmd == 'reset':
+                print 'reset slice: Not implemented'
+                return Response(status=404)
+            else:
+                return Response(status=404)
+            
+            flow = {'priority': 100, 'table_id':0}
+            flow['actions'] = make_vxlan_encap_action(
+                eth_src = slg[2].interface[2].mac_address,
+                eth_dst = slg[2].interface[2].gateway.mac_address,
+                ipv4_src = slg[2].interface[2].ip_address,
+                ipv4_dst = slg[0].interface[2].ip_address,
+                udp_src = 12345,
+                vni = 5000 + slice_id * 10
+            )
+            flow['actions'].append({"type":"OUTPUT", "port": 2})
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 6, "ipv4_src": dst_ip, "ipv4_dst": src_ip, "tcp_src": dst_port}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 17, "ipv4_src": dst_ip, "ipv4_dst": src_ip, "udp_src": dst_port}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
+            flow['match'] = {'in_port': 1, "dl_type": 2048, "ip_proto": 1, "ipv4_src": dst_ip, "ipv4_dst": src_ip}
+            print flow
+            _ofctl.mod_flow_entry(dp, flow, of_cmd)
 
         return Response(status=200)
     
